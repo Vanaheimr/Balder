ac9c6404bfe54f43d2ffea0595c88b4e9b6be11b
Achim Friedland <github@ahzf.de>
2011-10-13 23:52:11 +0200

    Some unittest cleanups...

--- UnitTests/TinkerGraphFactory.cs
+++ UnitTests/TinkerGraphFactory.cs
@@ -36,8 +36,18 @@ namespace de.ahzf.Blueprints.UnitTests
         {
 
             var _TinkerGraph = new PropertyGraph();
-            _TinkerGraph.OnVertexAdding += (graph, vertex, vote) => { Console.WriteLine("I like all vertices!"); };
-            _TinkerGraph.OnVertexAdding += (graph, vertex, vote) => { if (vertex.Id == 5) { Console.WriteLine("I'm a Jedi!"); vote.Veto(); } };
+
+            _TinkerGraph.OnVertexAdding += (graph, vertex, vote) => {
+                Console.WriteLine("I like all vertices!");
+            };
+
+            _TinkerGraph.OnVertexAdding += (graph, vertex, vote) =>
+            {
+                if (vertex.Id == 5) {
+                    Console.WriteLine("I'm a Jedi!");
+                    vote.Veto();
+                }
+            };
 
             var marko  = _TinkerGraph.AddVertex(1, v => v.SetProperty("name", "marko"). SetProperty("age",   29));
             var vadas  = _TinkerGraph.AddVertex(2, v => v.SetProperty("name", "vadas"). SetProperty("age",   27));
@@ -47,15 +57,20 @@ namespace de.ahzf.Blueprints.UnitTests
             var ripple = _TinkerGraph.AddVertex(6, v => v.SetProperty("name", "ripple").SetProperty("lang", "java"));
             var peter  = _TinkerGraph.AddVertex(7, v => v.SetProperty("name", "peter"). SetProperty("age",   35));
 
-            Console.WriteLine("Number of vertices added: " + _TinkerGraph.NumberOfVertices());
+            Console.WriteLine("Number of vertices added: " + _TinkerGraph.Vertices().Count());
 
-            marko.OnPropertyChanging += (sender, Key, oldValue, newValue, vote) => Console.WriteLine("'" + Key + "' property changing: '" + oldValue + "' -> '" + newValue + "'");
-            marko.OnPropertyChanged  += (sender, Key, oldValue, newValue)       => Console.WriteLine("'" + Key + "' property changed: '"  + oldValue + "' -> '" + newValue + "'");
+            marko.OnPropertyChanging += (sender, Key, oldValue, newValue, vote) =>
+                Console.WriteLine("'" + Key + "' property changing: '" + oldValue + "' -> '" + newValue + "'");
+
+            marko.OnPropertyChanged  += (sender, Key, oldValue, newValue)       =>
+                Console.WriteLine("'" + Key + "' property changed: '"  + oldValue + "' -> '" + newValue + "'");
+
 
             var _DynamicMarko = marko.AsDynamic();
             _DynamicMarko.age  += 100;
             _DynamicMarko.doIt  = (Action<String>) ((text) => Console.WriteLine("Some infos: " + text + "!"));
             _DynamicMarko.doIt(_DynamicMarko.name + "/" + marko.GetProperty("age") + "/");
+
 
             var e7  = _TinkerGraph.AddEdge(marko, vadas,  7,  "knows",   e => e.SetProperty("weight", 0.5));
             var e8  = _TinkerGraph.AddEdge(marko, josh,   8,  "knows",   e => e.SetProperty("weight", 1.0));
