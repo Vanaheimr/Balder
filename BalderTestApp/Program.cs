f52508f499b0989c871419a520ff21485b552cdc
Achim Friedland <github@ahzf.de>
2015-03-05 00:34:37 +0100

    u

---
+++ BalderTestApp/Program.cs
@@ -0,0 +1,61 @@
+﻿using org.GraphDefined.Vanaheimr.Balder;
+using org.GraphDefined.Vanaheimr.Balder.InMemory;
+using org.GraphDefined.Vanaheimr.Illias;
+using org.GraphDefined.Vanaheimr.Illias.Votes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalderTestApp
+{
+
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+
+            var graph = new GenericPropertyVertex<UInt64, Int64, String, String, Object,
+                                                  UInt64, Int64, String, String, Object,
+                                                  UInt64, Int64, String, String, Object,
+                                                  UInt64, Int64, String, String, Object>(1,
+
+                                                                                              // Vertices
+                                                                                              GraphDBOntology.Id.Suffix,
+                                                                                              GraphDBOntology.RevId.Suffix,
+                                                                                              GraphDBOntology.Label.Suffix,
+                                                                                              new IdGenerator_UInt64(),
+                                                                                              "VertexLabel",
+
+                                                                                              // Edges
+                                                                                              GraphDBOntology.Id.Suffix,
+                                                                                              GraphDBOntology.RevId.Suffix,
+                                                                                              GraphDBOntology.Label.Suffix,
+                                                                                              new IdGenerator_UInt64(),
+                                                                                              "EdgeLabel",
+
+                                                                                              // Multiedges
+                                                                                              GraphDBOntology.Id.Suffix,
+                                                                                              GraphDBOntology.RevId.Suffix,
+                                                                                              GraphDBOntology.Label.Suffix,
+                                                                                              new IdGenerator_UInt64(),
+                                                                                              "MultiEdgeLabel",
+
+                                                                                              // Hyperedges
+                                                                                              GraphDBOntology.Id.Suffix,
+                                                                                              GraphDBOntology.RevId.Suffix,
+                                                                                              GraphDBOntology.Label.Suffix,
+                                                                                              new IdGenerator_UInt64(),
+                                                                                              "HyperEdgeLabel",
+
+                                                                                              () => new VetoVote());
+
+
+        }
+
+    }
+
+}
